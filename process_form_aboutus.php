49291058aaa6d9edf3be390c854cbaa4b1c1405c
vishal-lumbhani <vl1crt@bolton.ac.uk>
2024-01-03 15:21:50 +0000

    first commit git bash

---
+++ process_form_aboutus.php
@@ -0,0 +1,34 @@
+﻿<!-- process_form.php -->
+<?php
+// Assuming you have a MySQL database
+$servername = "localhost";
+$username = "root";
+$password = "root@123";
+$dbname = "donation";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// SQL query to insert data into the database
+$sql = "INSERT INTO contact_form (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Form submitted successfully!";
+} else {
+    echo "Error: " . $sql . "
+<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
---
+++ process_form_request.php
@@ -0,0 +1,45 @@
+﻿<!-- process_form.php -->
+<?php
+// Assuming you have a MySQL database
+$servername = "localhost";
+$username = "root";
+$password = "root@123";
+$dbname = "donation";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve form data
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$mobileNumber = $_POST['mobileNumber'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$Postcode = $_POST['Postcode'];
+
+$Chair = $_POST['Chair'];
+$centertable = $_POST['centertable'];
+$toys = $_POST['toys'];
+$tvunit = $_POST['tvunit'];
+$fridge = $_POST['fridge'];
+$washingmachine = $_POST['washingmachine'];
+
+
+// SQL query to insert data into the database
+$sql = "INSERT INTO request_form (firstname, lastName, mobileNumber, address, city, Postcode, Chair, centertable, toys, tvunit,fridge,washingmachine) VALUES ('$firstName','$lastName','$mobileNumber','$address','$city','$Postcode','$Chair','$centertable','$toys','$tvunit', '$fridge', '$washingmachine')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Form submitted successfully!";
+} else {
+    echo "Error: " . $sql . "
+<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
